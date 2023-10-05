a7b5e934a4d862a2824977a318e1ca6f9b550bf6
pnouvellet <p.nouvellet@imperial.ac.uk>
2023-10-05 09:45:10 +0100

    Update project_fct.R

--- R/project_fct.R
+++ R/project_fct.R
@@ -21,24 +21,24 @@
 #' 
 #' 
 
-project_fct <- function(I0, Rt, n_sim, t_max, si, model = 'poisson',over = NULL){
+project_fct <- function(I0, Rt, n_loc, t_max, si, model = 'poisson',over = NULL){
   
-  I <- as.data.frame(matrix(NA,ncol = n_sim+1, nrow = t_max))
-  names(I) <- c('t',paste0('sim',1:n_sim))
+  I <- as.data.frame(matrix(NA,ncol = n_loc+1, nrow = t_max))
+  names(I) <- c('t',paste0('sim',1:n_loc))
   I$t <- 1:t_max
   
   # project
   if (model == 'poisson'){
     temp <- as.data.frame(project(x = I0,
                                   R = Rt$Rt, 
-                                  si = si[-1], n_sim = n_sim, time_change = 2:(t_max-0-I0$timespan),
+                                  si = si[-1], n_loc = n_loc, time_change = 2:(t_max-0-I0$timespan),
                                   n_days = t_max-I0$timespan, 
                                   R_fix_within = TRUE, 
                                   model = 'poisson',instantaneous_R = TRUE))
   }else if (model == 'negbin'){
     temp <- as.data.frame(project(x = I0,
                                   R = Rt$Rt, 
-                                  si = si[-1], n_sim = n_sim, time_change = 2:(t_max-0-I0$timespan),
+                                  si = si[-1], n_loc = n_loc, time_change = 2:(t_max-0-I0$timespan),
                                   n_days = t_max-I0$timespan, 
                                   R_fix_within = TRUE, 
                                   model = 'negbin',instantaneous_R = TRUE, size = over))
@@ -47,7 +47,7 @@ project_fct <- function(I0, Rt, n_sim, t_max, si, model = 'poisson',over = NULL)
   
   I[1:I0$timespan,] <- cbind(I0$dates,matrix(data = I0$counts,
                                              nrow = nrow(I0),
-                                             ncol = n_sim,byrow = FALSE))
+                                             ncol = n_loc,byrow = FALSE))
   
   I[(I0$timespan+1):(nrow(I)),] <- temp
   

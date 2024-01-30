c27f1e927ad6f95ac4bb116b27052624212632a8
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-01-30 11:15:04 +0000

    some check + correct likelihood poisson with rep
    likelihood with reporting, is not poisson if p_rep < 1!!!

--- R/Like1.R
+++ R/Like1.R
@@ -47,7 +47,15 @@ Like1 <- function(theta, data_long, t_window, n_loc, n_tw, param_agg = FALSE, ov
     logL_ind <- dnbinom(x = data_long$Inc_lk, mu = lambda, 
                         size = lambda^2/(varnb-lambda) , log = TRUE)
   }else{
-    logL_ind <- dpois(x = data_long$Inc_lk, lambda = lambda, log = TRUE)
+    if(p_reps==1){
+      logL_ind <- dpois(x = data_long$Inc_lk, lambda = lambda, log = TRUE)
+    }else{
+      varnb <- lambda*(1+(1-p_reps)* Rts_lk ) + 
+        (1-p_reps)* Rts_lk*(1+ Rts_lk )
+      # get the log-likelihood (minus constant bits)
+      logL_ind <- dnbinom(x = data_long$Inc_lk, mu = lambda, 
+                          size = lambda^2/(varnb-lambda) , log = TRUE)
+    }
   }
   
   

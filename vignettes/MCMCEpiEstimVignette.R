9b60c15256b51a3842626d0820879d924e41964d
pnouvellet <p.nouvellet@imperial.ac.uk>
2024-07-16 15:32:06 +0100

    update

---
+++ vignettes/MCMCEpiEstimVignette.R
@@ -0,0 +1,6 @@
+## ----install, eval=FALSE------------------------------------------------------
+#  install.packages("MCMCEpiEstim")
+
+## ----install2, eval=FALSE-----------------------------------------------------
+#  devtools::install_github("reconhub/MCMCEpiEstim")
+

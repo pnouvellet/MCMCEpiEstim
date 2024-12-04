c0f345b4210c8790846b8a639aa5f17d9ef5a8e8
cm401 <christian.morgenstern@gmail.com>
2024-12-04 15:22:05 +0000

    In truncate incidence account for the case with just a single location / time series.

--- R/truncate_incidence.R
+++ R/truncate_incidence.R
@@ -20,7 +20,12 @@ truncate_incidence <- function(I0_t_import, I, t_window,
   
   # check incidence/time to truncate
   if (!is.null(incidence_truncate)){
-    f <- apply(I[,-1],2,function(x) which(x>incidence_truncate)[1])
+    if(ncol(I)==2){
+      f <- which(I[,2]>incidence_truncate)[1]
+    } else {
+      f <- apply(I[,-1],2,function(x) which(x>incidence_truncate)[1])
+    }
+    
     f <- ceiling((f-I0_t_import)/7)*7 + I0_t_import+1
     for(i in 2:nrow(I)){
       if(!is.na(f[i-1])){
@@ -32,8 +37,6 @@ truncate_incidence <- function(I0_t_import, I, t_window,
   if (!is.null(t_truncate)){
     I <- I[1:(t_truncate*t_window+I0_t_import),]
   }
-  
- 
   
   return(I)
 }
